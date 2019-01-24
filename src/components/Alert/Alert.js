ed46b7ae4fc84aad6a9111250fea0ea5feb32a21
Stephanie Condrey <condrey_stephanie@bah.com>
2019-01-23 20:57:24 -0500

    Added alert

---
+++ src/components/Alert/Alert.js
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Alert = styled.div``
+
+Alert.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Alert
